9fc31a61079dc7e3e0ec996833adb278991bb213
ugyentring <tsheringugyen483@gmail.com>
2025-08-26 01:02:29 +0600

    add the basic get request successfully with hardcoded data

--- GameStoreApi/Program.cs
+++ GameStoreApi/Program.cs
@@ -1,5 +1,33 @@
+using GameStoreApi.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+//list of games hardcoded
+List<GameDto> games = [
+    new(
+        1,
+        "PubG",
+        "War",
+        19.9M,
+        new DateOnly(2015, 7, 17)),
+
+    new(
+        2,
+        "MLBB",
+        "Fighting",
+        12.89M,
+        new DateOnly(2010, 9, 30)),
+
+    new(
+        3,
+        "Freefire",
+        "Fire",
+        13.99M,
+        new DateOnly(2015, 9, 21))
+];
+
+app.MapGet("games", () => games);
 
 app.MapGet("/", () => "Hello Ugyen!");
 
